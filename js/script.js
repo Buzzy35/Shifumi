137ea961daaf2edfe293c2294c3222191feac909
Buzzy35 <imbuzzy35@gmail.com>
2025-09-16 14:40:49 +0200

    ajout d un easter egg

--- js/script.js
+++ js/script.js
@@ -3,9 +3,20 @@ const winDisplay = document.getElementById("win");
 const drawDisplay = document.getElementById("draw");
 const loseDisplay = document.getElementById("lose");
 const resetButton = document.getElementById("restart");
+const resultatDisplay = document.getElementById("resultat");
 
 const choix = ["Pierre", "Feuille", "Ciseaux"];
 const score = {win: 0, draw: 0, lose: 0};
+
+const konami = [
+    "ArrowUp", "ArrowUp",
+    "ArrowDown", "ArrowDown",
+    "ArrowLeft", "ArrowRight",
+    "ArrowLeft", "ArrowRight",
+    "b", "a"
+];
+
+let input = [];
 
 function update() {
     winDisplay.textContent = score["win"] > 1 ? score["win"] + " victoires" : score["win"] + " victoire";
@@ -26,10 +37,16 @@ buttons.forEach(button => {
             choixJoueur === "Feuille" && choixRobot === "Pierre" ||
             choixJoueur === "Ciseaux" && choixRobot === "Feuille") {
             resultat = "win";
+
         } else {
             resultat = "lose";
         }
 
+        resultatDisplay.textContent = "Vous avez jouer " + choixJoueur + " contre " + choixRobot + " et vous avez " + resultat + " ";
+        resultatDisplay.classList.add("scale-up-center")
+        setTimeout(() => {
+            resultatDisplay.classList.remove("scale-up-center")
+        }, 1000);
         score[resultat]++;
         update();
     })
@@ -52,3 +69,21 @@ buttons.forEach(button => {
 })
 
 
+document.addEventListener("keydown", function (event) {
+    input.push(event.key);
+
+    if (input.length > konami.length) {
+        input.shift();
+    }
+
+    if (input.join(",") === konami.join(",")) {
+        const choixRobot = choix[Math.floor(Math.random() * 3)];
+        score["win"]++;
+        update()
+        resultatDisplay.textContent = "Vous avez jouer le Puits contre " + choixRobot + " et vous avez tricher et gangner";
+        resultatDisplay.classList.add("scale-up-center")
+        setTimeout(() => {
+            resultatDisplay.classList.remove("scale-up-center")
+        }, 1000);
+    }
+});
